9667aab31253d566814235f84676cd426e148864
Subhanshu20101 <76595496+Subhanshu20101@users.noreply.github.com>
2021-06-27 18:38:27 +0530

    Update urls.py

--- podcast/urls.py
+++ podcast/urls.py
@@ -19,10 +19,8 @@ from django.urls.conf import include
 
 #y????????????????/
 
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
